5dede1188f8b67fa66aeab40e4ee91960855b54b
lucianamarques <lucianadacostamarques@gmail.com>
2018-12-17 13:43:29 -0200

    Erase View Controller

---
+++ View Switcher/SwitchingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SwitchingViewController.swift
+//  View Switcher
+//
+//  Created by Luciana on 17/12/18.
+//  Copyright © 2018 Luciana. All rights reserved.
+//
+
+import UIKit
+
+class SwitchingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
