ec3d116f7e844087c573262d25154ab3a88f230a
lucianamarques <lucianadacostamarques@gmail.com>
2018-12-26 15:04:17 -0200

    project working

--- View Switcher/SwitchingViewController.swift
+++ View Switcher/SwitchingViewController.swift
@@ -17,8 +17,8 @@ class SwitchingViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         blueViewController =
-            storyboard?.instantiateViewController(withIdentifier: "Blue")
-            as? BlueViewController
+            (storyboard?.instantiateViewController(withIdentifier: "Blue")
+                as! BlueViewController)
         blueViewController.view.frame = view.frame
         switchViewController(from: nil, to: blueViewController)  // helper method
     }
@@ -51,29 +51,38 @@ class SwitchingViewController: UIViewController {
     }
     
     @IBAction func switchViews(sender: UIBarButtonItem){
-        // Create View Controller, if required
-        if yellowViewController.view.superview == nil {
+        // Create the new view controller, if required
+        if yellowViewController?.view.superview == nil {
             if yellowViewController == nil {
                 yellowViewController =
-                    storyboard?.instantiateViewController(withIdentifier: "Yellow") as? YellowViewController
+                    (storyboard?.instantiateViewController(withIdentifier: "Yellow")
+                        as! YellowViewController)
             }
-        } else if blueViewController.view.superview == nil {
+        } else if blueViewController?.view.superview == nil {
             if blueViewController == nil {
-                blueViewController = storyboard?.instantiateViewController(withIdentifier: "Blue") as? BlueViewController
-            }
-        }
-        
-        // Switch View Controllers
+                blueViewController =
+                    (storyboard?.instantiateViewController(withIdentifier: "Blue")
+                        as! BlueViewController)
+            } }
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        // Switch view controllers
         if blueViewController != nil
             && blueViewController!.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight,
+                                          for: view, cache: true)
             yellowViewController.view.frame = view.frame
             switchViewController(from: blueViewController,
                                  to: yellowViewController)
         } else {
+            UIView.setAnimationTransition(.flipFromLeft,
+                                          for: view, cache: true)
             blueViewController.view.frame = view.frame
             switchViewController(from: yellowViewController,
                                  to: blueViewController)
         }
+        UIView.commitAnimations()
     }
 
     /*
